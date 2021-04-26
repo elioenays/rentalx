3dd57723291c2035ffc7f809f7db942f552797ef
Elioenay Silva <elionay407@gmail.com>
2021-04-25 22:44:37 -0300

    Listando categorias

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../model/category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+}
