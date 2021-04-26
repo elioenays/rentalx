755f3c810c238225becfb9f73924b38a4f0297f4
Elioenay Silva <elionay407@gmail.com>
2021-04-25 22:24:35 -0300

    Inseridno tipagem para caregoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/category";
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = { id: uuidV4(), name, description, created_at: Date.now() };
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).json(categories);
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
