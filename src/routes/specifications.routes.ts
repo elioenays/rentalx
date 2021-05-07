01e6b8638845368306b3e3b7fea5ef972e1ec6c8
Elioenay Silva <elionay407@gmail.com>
2021-05-07 00:06:11 -0300

    Criando use case de especificação

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRouter = Router();
