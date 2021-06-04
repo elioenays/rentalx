33db548732bcd30b2987142ccf03c1127dfab485
Elioenay Silva <elionay407@gmail.com>
2021-06-03 23:29:01 -0300

    Refatorando Specificações

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
   handle(request: Request, response: Response) {
     const { name, description } = request.body;
 
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase
+    );
+
     const specification = { name, description };
 
-    this.createSpecificationUseCase.execute(specification);
+    createSpecificationUseCase.execute(specification);
 
     return response.status(201).json(specification);
   }
