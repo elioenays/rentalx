0ee297afc503e2405f51b8119aaf7ea1ac700330
Elioenay Silva <elionay407@gmail.com>
2021-07-13 16:01:52 -0300

    Iniciando caso de uso da entidade carros

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,16 @@
+import { injectable } from "tsyringe";
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+@injectable()
+export class CreateCarUseCase {
+  async execute(): Promise<void> {}
+}
